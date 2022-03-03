db300831dcdd9bc071b1329b1f2a51764e20f66b
clagnar <clarisse.gagnebin@heig-vd.ch>
2022-03-03 16:00:08 +0100

    "Completé les derniers exercices"

--- 01-SVG/js/script.js
+++ 01-SVG/js/script.js
@@ -1,7 +1,20 @@
+const rectangles = document.querySelectorAll('.rectangle')
 
-let rect = document.querySelectorAll('rect');
+rectangles.forEach(rectangle => {
+    rectangle.addEventListener('click', evt => {
+        evt = evt.target;
+        console.log(evt);
+        rectangle.classList.toggle('Change')
+    })
+})
 
+const donuts = document.querySelectorAll('.donut')
 
-rect.addEventListener("click", evt =>{
-    rect.classList.toggle('lightblue');
+donuts.forEach(donut => {
+    donut.addEventListener('mouseover', evt => {
+        evt = evt.target;
+        console.log(evt);
+        /* donut.setAttribute("r","100") */
+        donut.classList.toggle('changerayon')
+    })
 })
