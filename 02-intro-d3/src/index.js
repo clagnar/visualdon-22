398bb6afdebee922c5a9ac710844286a52314028
clagnar <clarisse.gagnebin@heig-vd.ch>
2022-03-11 13:19:59 +0100

    ExercicesSemaine2

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -1,4 +1,29 @@
 import * as d3 from 'd3';
 
-// C'est ici que vous allez écrire les premières lignes en d3!
+//Changer la couleur du deuxième cercle
+const svg = d3.select(".mon-svg");
+const cercle_dos = svg.select("#dos");
+cercle_dos.attr("fill", "#E92528");
 
+//Déplacez de 50px vers la droite le premier et le deuxième cercle
+const cercles_translation = svg.selectAll(".translation");
+cercles_translation.attr('transform','translate(50,0)');
+
+//Rajoutez du texte en dessous de chaque cercle
+d3.selectAll(".circle")
+  .append("g")
+  .attr("class", "circleLabel")
+  .append("text")
+  .text(function(d) {
+    return d.label
+  })
+
+  //Alignez verticalement les cercles en cliquant sur le dernier cercle
+  const cercle_3 = svg.select('#tres');
+  let r = 15;
+  cercle_3.on("click", () => {
+        r = r + 10;
+        cercle_3.attr("r", r);
+        } )
+
+  
